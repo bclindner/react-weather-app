7c8b67f8c8a0edb5ccea78e6ba737d5cbb2fbc6f
Brian C. Lindner <brian@bclindner.com>
2018-09-09 17:57:00 -0400

    refactor; further componentization and now adhering to standardJS style

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A simple single-page React app that pulls weather information from OpenWeatherMa
 
 Uses [React](http://reactjs.org/) and [OpenWeatherMap](https://openweathermap.org/), of course, as well as [Babel](https://babeljs.io/) for in-browser conversion from JSX to JS, and [Bootstrap](http://getbootstrap.com/) for general layout and looks. Slapped together in a few hours to get the gist of React. Makes absolutely no attempt at best practices. Especially for images. Hope you like pixelation.
 
-This repo is set up with a dummy key, so it won't work if you just try it as-is - to use the app, deploy it to a server and change the 'apiKey' global variable in `weatherapp.jsx` to a valid [OpenWeatherMap API key](https://openweathermap.org/api) (which you can get for free!).
+This repo is not set up with a key so it won't work if you just try it as-is - to use the app, you'll have to set the apiKey prop for the WeatherApp component in the `weatherapp.jsx` file to a valid [OpenWeatherMap API key](https://openweathermap.org/api) (which you can get for free!).
--- weatherapp.jsx
+++ weatherapp.jsx
@@ -1,19 +1,19 @@
 // Example data.
 const example = {
-    "dt": 0,
-    "weather": [
-        {
-            "main": "...",
-            "description": "...",
-            "icon": "01d"
-        }
-    ],
-    "main": {
-        "temp": 0,
-        "temp_min": 0,
-        "temp_max": 0
-    },
-    "name": "...",
+  'dt': 0,
+  'weather': [
+    {
+      'main': '...',
+      'description': '...',
+      'icon': '01d'
+    }
+  ],
+  'main': {
+    'temp': 0,
+    'temp_min': 0,
+    'temp_max': 0
+  },
+  'name': '...'
 }
 
 // Converts Kelvin to Celsius.
@@ -25,12 +25,9 @@ function toFahrenheit (temp) {
   return ((temp - 273.15) * (9 / 5) + 32).toFixed(1)
 }
 
-const apiKey = "deadbeefdeadbeefdeadbeefdeadbeef"
-
 class WeatherApp extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
-    this.apiKey = apiKey
     this.state = {
       zip: 30458,
       currentWeather: example,
@@ -39,53 +36,72 @@ class WeatherApp extends React.Component {
     this.handleZIPChange = this.handleZIPChange.bind(this)
   }
   update () {
-    fetch('https://api.openweathermap.org/data/2.5/weather?appid=' + this.apiKey + '&zip=' + this.state.zip)
+    fetch('https://api.openweathermap.org/data/2.5/weather?appid=' + this.props.apiKey + '&zip=' + this.state.zip)
       .then(res => res.json())
       .then(json => this.setState({currentWeather: json}))
-    fetch('https://api.openweathermap.org/data/2.5/forecast?appid=' + this.apiKey + '&zip=' + this.state.zip)
+    fetch('https://api.openweathermap.org/data/2.5/forecast?appid=' + this.props.apiKey + '&zip=' + this.state.zip)
       .then(res => res.json())
       .then(json => this.setState({forecast: json.list}))
   }
-  handleZIPChange(newZip) {
+  handleZIPChange (newZip) {
     this.setState({
       zip: newZip
     }, this.update)
   }
-  componentDidMount() {
+  componentDidMount () {
     this.update()
   }
   render () {
     return (
-      <div class="container border shadow my-sm-3 p-3">
-        <header class="row">
-          <div class="col-md-3">
-            <h1 class="text-center text-md-left font-weight-light">Weather</h1>
-          </div>
-          <div class="col-md-9">
-            <WeatherSearchBar handleZIPChange={this.handleZIPChange}/>
-          </div>
-        </header>
+      <div class='container border shadow my-3 p-3'>
+        <WeatherHeader handleZIPChange={this.handleZIPChange} />
         <hr />
-        <div class="row">
-          <div class="col-md-3">
-            <WeatherIcon id={this.state.currentWeather.weather[0].icon} />
-          </div>
-          <div class="col-md-9">
-            <WeatherDetails data={this.state.currentWeather} />
-          </div>
-        </div>
+        <WeatherDisplay currentWeather={this.state.currentWeather} />
         <hr />
-        <div class="row">
-          <div class="col p-3">
-            <h1 class="font-weight-light">Forecast</h1>
-          </div>
+        <WeatherForecastList data={this.state.forecast} />
+        <hr />
+        <WeatherFooter />
+      </div>
+    )
+  }
+}
+
+class WeatherFooter extends React.Component {
+  render () {
+    return (
+      <div class='row'>
+        <div class='col'>
+          <p class='text-center'>App by Brian Lindner, 2018. Made with <a href='https://reactjs.org'>React</a> and <a href='https://getbootstrap.com'>Bootstrap</a>, and the <a href='https://openweathermap.org/'>OpenWeatherMap</a> API. </p>
         </div>
-        <WeatherForecastList data={this.state.forecast}/>
-        <hr/>
-        <div class="row">
-          <div class="col">
-            <p class="text-center">App by Brian Lindner, 2018</p>
-          </div>
+      </div>
+    )
+  }
+}
+
+class WeatherHeader extends React.Component {
+  render () {
+    return (
+      <header class='row'>
+        <div class='col-md-3'>
+          <h1 class='text-center text-md-left font-weight-light'>Weather</h1>
+        </div>
+        <div class='col-md-9'>
+          <WeatherSearchBar handleZIPChange={this.props.handleZIPChange} />
+        </div>
+      </header>
+    )
+  }
+}
+
+class WeatherDisplay extends React.Component {
+  render () {
+    return (
+      <div class='row'>
+        <div class='col-md-3'>
+          <WeatherIcon id={this.props.currentWeather.weather[0].icon} />
+        </div>
+        <div class='col-md-9'>
+          <WeatherDetails data={this.props.currentWeather} />
         </div>
       </div>
     )
@@ -93,14 +109,14 @@ class WeatherApp extends React.Component {
 }
 
 class WeatherSearchBar extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.verify = this.verify.bind(this)
   }
-  verify(event) {
+  verify (event) {
     let el = event.target
     let value = el.value
-    if (value.length != 5){
+    if (value.length != 5) {
       el.classList.add('is-invalid')
     } else {
       el.classList.remove('is-invalid')
@@ -109,7 +125,7 @@ class WeatherSearchBar extends React.Component {
   }
   render () {
     return (
-      <input class="form-control form-control-lg text-center text-md-left" onInput={this.verify} maxLength="5" placeholder="Type in a ZIP code..." />
+      <input class='form-control form-control-lg text-center text-md-left' onInput={this.verify} maxLength='5' placeholder='Type in a ZIP code...' />
     )
   }
 }
@@ -134,12 +150,12 @@ class WeatherDetails extends React.Component {
       }
     }
     return (
-      <div class="col-md-9">
-        <h4 class="text-center text-md-left">{this.props.data.name}</h4>
-        <h1 class="text-center text-md-left">{this.props.data.weather[0].main}</h1>
-        <h2 class="text-center text-md-left">{temp.current.f}&deg;F <small class="font-weight-light">{temp.current.c}&deg;C</small></h2>
-        <h2 class="text-center text-md-left">High {temp.high.f}&deg;F <small class="font-weight-light">{temp.high.c}&deg;C</small></h2>
-        <h2 class="text-center text-md-left">Low {temp.low.f}&deg;F <small class="font-weight-light">{temp.low.c}&deg;C</small></h2>
+      <div class='col-md-9'>
+        <h4 class='text-center text-md-left'>{this.props.data.name}</h4>
+        <h1 class='text-center text-md-left'>{this.props.data.weather[0].main}</h1>
+        <h2 class='text-center text-md-left'>{temp.current.f}&deg;F <small class='font-weight-light'>{temp.current.c}&deg;C</small></h2>
+        <h2 class='text-center text-md-left'>High {temp.high.f}&deg;F <small class='font-weight-light'>{temp.high.c}&deg;C</small></h2>
+        <h2 class='text-center text-md-left'>Low {temp.low.f}&deg;F <small class='font-weight-light'>{temp.low.c}&deg;C</small></h2>
       </div>
     )
   }
@@ -149,27 +165,34 @@ class WeatherIcon extends React.Component {
   render () {
     return (
       <div >
-        <img src={'https://openweathermap.org/img/w/' + this.props.id + '.png'} width="350" height="350" class="img-fluid mx-auto d-block" />
+        <img src={'https://openweathermap.org/img/w/' + this.props.id + '.png'} width='350' height='350' class='img-fluid mx-auto d-block' />
       </div>
     )
   }
 }
 
 class WeatherForecastList extends React.Component {
-  render() {
+  render () {
     return (
-      <div class="row">
-        <div class="col-md-3 my-3">
-          <WeatherForecastCard data={this.props.data[0]} />
+      <div>
+        <div class='row'>
+          <div class='col p-3'>
+            <h1 class='font-weight-light'>Forecast</h1>
+          </div>
         </div>
-        <div class="col-md-3 my-3">
-          <WeatherForecastCard data={this.props.data[1]} />
-        </div>
-        <div class="col-md-3 my-3">
-          <WeatherForecastCard data={this.props.data[2]} />
-        </div>
-        <div class="col-md-3 my-3">
-          <WeatherForecastCard data={this.props.data[3]} />
+        <div class='row'>
+          <div class='col-md-3 my-3'>
+            <WeatherForecastCard data={this.props.data[0]} />
+          </div>
+          <div class='col-md-3 my-3'>
+            <WeatherForecastCard data={this.props.data[1]} />
+          </div>
+          <div class='col-md-3 my-3'>
+            <WeatherForecastCard data={this.props.data[2]} />
+          </div>
+          <div class='col-md-3 my-3'>
+            <WeatherForecastCard data={this.props.data[3]} />
+          </div>
         </div>
       </div>
     )
@@ -177,7 +200,7 @@ class WeatherForecastList extends React.Component {
 }
 
 class WeatherForecastCard extends React.Component {
-  render() {
+  render () {
     let temp = {
       high: {
         f: toFahrenheit(this.props.data.main.temp_max),
@@ -190,14 +213,14 @@ class WeatherForecastCard extends React.Component {
     }
     let hour = new Date(this.props.data.dt * 1000).getHours()
     return (
-      <div class="card">
-        <div class="card-header text-center"><h3 class="font-weight-light">{hour}:00</h3></div>
-        <div class="card-body">
-          <div class="text-center">
-            <WeatherIcon id={this.props.data.weather[0].icon}/>
+      <div class='card'>
+        <div class='card-header text-center'><h3 class='font-weight-light'>{hour}:00</h3></div>
+        <div class='card-body'>
+          <div class='text-center'>
+            <WeatherIcon id={this.props.data.weather[0].icon} />
             <h4>{this.props.data.weather[0].main}</h4>
             <h4>{temp.low.f}&deg;F / {temp.high.f}&deg;F</h4>
-            <h5 class="font-weight-light">{temp.low.c}&deg;C / {temp.high.c}&deg;C</h5>
+            <h5 class='font-weight-light'>{temp.low.c}&deg;C / {temp.high.c}&deg;C</h5>
           </div>
         </div>
       </div>
@@ -206,6 +229,6 @@ class WeatherForecastCard extends React.Component {
 }
 
 ReactDOM.render(
-  <WeatherApp />,
+  <WeatherApp apiKey='' />,
   document.getElementById('react-container')
 )
